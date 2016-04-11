10c046d5cea6f9db34b39ecfe12bf55bcc37a4a4
Holger Esser (hoesser) <hoesser@cisco.com>
2016-04-11 10:57:35 -0500

    chat

--- templates/customer.php
+++ templates/customer.php
@@ -43,7 +43,8 @@
         <div class="col-md-4">
           <h2>TAC</h2>
           <p></p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <!-- <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p> -->
+		  <p><a class="btn btn-default" href="http://76.204.214.224/candy/example/index.html"  onclick="window.open('print.html', 'newwindow', 'width=300, height=250'); return false;"> Print</a></p>
        </div>
         <div class="col-md-4">
           <h2>General Inquiries</h2>
